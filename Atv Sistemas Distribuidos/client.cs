631ed7e2f656293b6b0501c05325c6dea67e7cd8
LipeMachado <silvalipe765@gmail.com>
2023-09-24 12:51:57 -0300

    Init Commit

---
+++ Atv Sistemas Distribuidos/client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class client
+{
+    static void Main()
+    {
+        try
+        {
+            // Conecte-se ao servidor.
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 12345);
+            Console.WriteLine("Conectado ao servidor...");
+
+            while (true)
+            {
+                // Solicite a operação ao usuário.
+                Console.Write("Digite a operação (ex. 5 + 3): ");
+                string operacao = Console.ReadLine();
+
+                // Envie a operação para o servidor.
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.ASCII.GetBytes(operacao);
+                stream.Write(data, 0, data.Length);
+
+                // Receba a resposta do servidor e imprima na tela.
+                byte[] response = new byte[1024];
+                int bytesRead = stream.Read(response, 0, response.Length);
+                string resultado = Encoding.ASCII.GetString(response, 0, bytesRead);
+                Console.WriteLine("Resultado: " + resultado);
+            }
+
+            // Encerre a conexão com o servidor.
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro: " + ex.Message);
+        }
+    }
+}
---
+++ Atv Sistemas Distribuidos/server.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class server
+{
+    static void Main()
+    {
+        // Defina o endereço IP e a porta em que o servidor escutará as conexões.
+        int port = 12345;
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        IPEndPoint endPoint = new IPEndPoint(ipAddress, port);
+
+        // Crie um socket TCP para o servidor.
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Faça o bind do socket ao ponto de extremidade.
+        serverSocket.Bind(endPoint);
+
+        // Comece a escutar por conexões.
+        serverSocket.Listen(10);
+        Console.WriteLine("Servidor iniciado...");
+
+        // Aguarde por uma conexão do cliente.
+        Socket clientSocket = serverSocket.Accept();
+        Console.WriteLine("Cliente conectado...");
+
+        while (true)
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead = clientSocket.Receive(buffer);
+
+            // Decodifique os dados recebidos.
+            string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Recebido: " + data);
+
+            // Realize a operação e envie a resposta de volta para o cliente.
+            double resultado = Calcular(data);
+            byte[] response = Encoding.ASCII.GetBytes(resultado.ToString());
+            clientSocket.Send(response);
+            Console.WriteLine("Enviado: " + resultado);
+        }
+
+        // Encerre a conexão com o cliente e pare o servidor.
+        clientSocket.Close();
+        serverSocket.Close();
+    }
+
+    static double Calcular(string expressao)
+    {
+        string[] partes = expressao.Split(' ');
+        double numero1 = Convert.ToDouble(partes[0]);
+        double numero2 = Convert.ToDouble(partes[2]);
+        string operacao = partes[1];
+
+        double resultado = 0;
+        switch (operacao)
+        {
+            case "+":
+                resultado = numero1 + numero2;
+                break;
+            case "-":
+                resultado = numero1 - numero2;
+                break;
+            case "*":
+                resultado = numero1 * numero2;
+                break;
+            case "/":
+                resultado = numero1 / numero2;
+                break;
+        }
+
+        return resultado;
+    }
+}
